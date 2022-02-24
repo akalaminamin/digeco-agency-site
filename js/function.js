f67c3be82f54a2f05f53cf2e5ddb031ba5a0224b
Al Amin <akalaminamin91@gmail.com>
2022-02-23 21:18:14 -0800

    setup responsive all section without header

--- js/function.js
+++ js/function.js
@@ -1,35 +1,36 @@
 // Use mixitup plugin
-let mixer = mixitup(".mix-container")
+let mixer = mixitup(".mix-container");
 
 // Use AOS plugin
 AOS.init({
-    offset: 400,
-    duration: 3000
+  offset: 400,
+  duration: 3000,
 });
 
 // Add class for sticky menu
 let stickyHeader = document.querySelector("header");
-
+let logo = document.querySelector("#logo");
 window.onscroll = () => {
-    if (this.scrollY > 50) {
-        // add class
-        stickyHeader.classList.add("sticky")
-    } else {
-        // remove class
-        stickyHeader.classList.remove("sticky");
-    }
-}
+  if (this.scrollY > 50) {
+    // add class
+    stickyHeader.classList.add("sticky");
+    logo.src = "Images/logo.png";
+  } else {
+    // remove class
+    stickyHeader.classList.remove("sticky");
+    logo.src = "Images/logo2.png";
+  }
+};
 
-// declear variable 
+// declear variable
 let btnParrent = document.querySelector(".btn-group");
 let btnChild = document.querySelectorAll(".btn-group .btn");
-
-// Add Class array function 
-btnChild.forEach(e => {
-    e.addEventListener("click", () => {
-        // remove class
-        btnParrent.querySelector(".active").classList.remove("active");
-        // add class
-        e.classList.add("active");
-    })
+// Add Class array function
+btnChild.forEach((e) => {
+  e.addEventListener("click", () => {
+    // remove class
+    btnParrent.querySelector(".active").classList.remove("active");
+    // add class
+    e.classList.add("active");
+  });
 });
